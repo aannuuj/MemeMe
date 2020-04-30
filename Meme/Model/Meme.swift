8a6d6c787fddb946bf2c74a5f32f3e001540b9f4
aannuuj <hariompalkar@gmail.com>
2020-04-30 18:20:17 +0530

    initial commit

---
+++ Meme/Model/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Hariom Palkar on 29/04/20.
+//  Copyright © 2020 Hariom Palkar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String = "TOP"
+    var bottomText: String = "BOTTOM"
+    var originalImage: UIImage?
+    var memeImage: UIImage?
+}
---
+++ Meme/Utilities/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  MemeMe
+//
+//  Created by Aaryan Kothari on 30/04/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+//MARK:- General Extensions
+
+//MARK: hide keyboard on tap
+extension UIViewController {
+    
+    // Function for tap gesture
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true) ///selector action to dissmiss keyboard
+    }
+}
